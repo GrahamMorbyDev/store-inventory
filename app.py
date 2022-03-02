e3bddefd74a8149c0d5cdaf1aaab33edc085a5ce
Graham Morby <graham.morby@leighton.com>
2022-03-02 13:26:09 +0000

    added models file and set up with app now having a menu and app function

--- app.py
+++ app.py
@@ -1,18 +1,90 @@
-from sqlalchemy import create_engine, Column, Integer, String, FLOAT, DATETIME
-from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine('sqlite:///inventory.db', echo=True)
-Base = declarative_base()
+from models import (Base, session, Product, engine)
+import csv
+import datetime
 
 
-class Product(Base):
-    __tablename__ = 'products'
-    id = Column(Integer, primary_key=True)
-    product_name = Column(String)
-    product_quantity = Column(Integer)
-    product_price = Column(FLOAT)
-    date_updated = Column(DATETIME)
+def clean_price(price_str):
+    split_price = price_str.split('$')
+    try:
+        new_price = float(split_price[1])
+    except IndexError:
+        input('''\nThe amount you entered is not correct, please try again
+                 \r Example: $10.99
+                 \r Press ENTER to continue''')
+    else:
+        return int(new_price*100)
+
+
+def clean_date(date_str):
+    split_date = date_str.split('/')
+    try:
+        month = int(split_date[1])
+        day = int(split_date[0])
+        year = int(split_date[2])
+        return_date = datetime.date(year, month, day)
+    except ValueError:
+        input('''
+                \n******* DATE ERROR *******
+                \rThe data format should include a valid Day, Month and Year 
+                \rExample: 10/07/1982
+                \rPress enter to try again
+                \r**************************
+            ''')
+    else:
+        return return_date
+
+
+def add_csv_data():
+    with open('inventory.csv') as csvfile:
+        data = csv.reader(csvfile)
+        for row in data:
+            product_in_db = session.query(Product).filter(Product.product_name == row[0]).one_or_none()
+            if product_in_db is None:
+                product_name = row[0]
+                product_quantity = row[2]
+                product_price = clean_price(row[1])
+                date_updated = clean_date(row[3])
+                new_product = Product(product_name=product_name,
+                                      product_quantity=product_quantity,
+                                      product_price=product_price,
+                                      date_updated=date_updated)
+                session.add(new_product)
+        session.commit()
+
+
+def menu():
+    while True:
+        print('''
+            \nPRODUCTS DATABASE
+            \rv: View all products
+            \ra: Add a new product
+            \rb: Back up database
+            \rq: Exit
+        ''')
+        choice = input('What would you like to do? ')
+        if choice.lower() in ['v', 'a', 'b', 'q']:
+            return choice.lower()
+        else:
+            input('''\rPlease choice one the options above
+                     \reither v, a, b, or q.
+                     \rPress enter to try again  ''')
+
+
+def app():
+    running = True
+    while running:
+        choice = menu()
+        if choice == 'v':
+            pass
+        elif choice == 'a':
+            pass
+        elif choice == 'b':
+            pass
+        else:
+            print('Goodbye and thanks for using this tool')
+            exit()
 
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    add_csv_data()
---
+++ models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, Column, Integer, String, DATETIME
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///inventory.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True)
+    product_name = Column(String)
+    product_quantity = Column(Integer)
+    product_price = Column(Integer)
+    date_updated = Column(DATETIME)
+
+
+def __repr__(self):
+    print(f'Product name: {self.product_name}, Quantity: {self.product_quantity}, Price: {self.product_price}, Updated: {self.date_updated}')
